1224bcf874148b7c1fbdf412f111047a6dc303ac
Ramon Jardí <risenjardi@gmail.com>
2025-10-01 18:47:10 +0200

    ui: finish location page

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        location: resolve(__dirname, 'src/pages/location.html'),
+      },
+    },
   },
   server: {
     open: '/index.html',
