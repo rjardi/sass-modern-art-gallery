960b6458af99b9ed2e9b098d70b486c8af9ca26f
rjardi <risenjardi@gmail.com>
2026-03-01 20:29:05 +0100

    fix: simplificar rutas moviendo location.html a la raíz

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        location: resolve(__dirname, 'src/pages/location.html'),
+        location: resolve(__dirname, 'location.html'),
       },
     },
   },
